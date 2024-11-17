1a06e573103fac3679164f7b4a6c301e51ebb526
joyfz20 <164316281+joyfz20@users.noreply.github.com>
2024-11-17 16:45:47 +0800

    added Status

--- ReplenishmentRequest.java
+++ ReplenishmentRequest.java
@@ -2,13 +2,15 @@ public class ReplenishmentRequest {
     private String medicineName;
     private int requestedQuantity;
     private String pharmacistId;
-    private boolean isApproved;
+    // private boolean isApproved;
+    private Status status;
 
     public ReplenishmentRequest(String medicineName, int requestedQuantity, String pharmacistId) {
         this.medicineName = medicineName;
         this.requestedQuantity = requestedQuantity;
         this.pharmacistId = pharmacistId;
-        this.isApproved = false;
+        // this.isApproved = false;
+        this.status = Status.PENDING;
     }
 
     // Getters
@@ -24,13 +26,24 @@ public class ReplenishmentRequest {
         return pharmacistId;
     }
 
-    public boolean isApproved() {
-        return isApproved;
+    /*
+     * public boolean isApproved() {
+     * return isApproved;
+     * }
+     */
+
+    public Status getStatus() {
+        return status;
     }
 
     // Approve the request
     public void approveRequest() {
-        isApproved = true;
+        // isApproved = true;
+        this.status = Status.COMPLETED;
+    }
+
+    public void declineRequest() {
+        this.status = Status.DECLINED;
     }
 
     @Override
@@ -39,7 +52,8 @@ public class ReplenishmentRequest {
                 "medicineName='" + medicineName + '\'' +
                 ", requestedQuantity=" + requestedQuantity +
                 ", pharmacistName='" + pharmacistId + '\'' +
-                ", isApproved=" + isApproved +
+                // ", isApproved=" + isApproved +
+                ", Status=" + status +
                 '}';
     }
 }
